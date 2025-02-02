1580be6b99a82dc8d1467bc8891476d4075d53db
ayush <flamedeveloper2021@gmail.com>
2025-02-02 21:04:26 +0530

    Implemented User Onboarding Form.

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -1,9 +1,168 @@
-import React from 'react'
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { onboardingSchema } from "@/app/lib/schema";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 const OnboardingForm = ({industries}) => {
+
+  const [selectedIndustry, setSelectedIndustry] = useState(null);
+  const router = useRouter();
+
+  const { 
+    register,
+    handleSubmit,
+    formState:{errors},
+    setValue,
+    watch,
+  } = useForm({
+    resolver: zodResolver(onboardingSchema)
+  })
+
+  const onSubmit = async (values) => {
+    console.log(values);
+  }
+
+  const watchIndustry = watch("industry");
+
   return (
-    <div>OnboardingForm</div>
+    <div className="flex items-center justify-center bg-background">
+      <Card className="w-full max-w-lg mt-10 mx-2">
+        <CardHeader>
+          <CardTitle className="gradient-title text-4xl">Complete Your Profile</CardTitle>
+          <CardDescription>Please select your industry to unlock personalized career insights and recommendations.</CardDescription>
+        </CardHeader>
+        <CardContent>
+
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+
+            {/* Industries */}
+            <div className="space-y-2">
+              <Label htmlFor="industry">Industry</Label>
+              <Select
+                onValueChange={(value) => {
+                  setValue("industry",value);
+                  setSelectedIndustry(
+                    industries.find((ind) => ind.id === value)
+                  );
+                  setValue("subIndustry","");
+                }}
+              >
+                <SelectTrigger id="industry">
+                  <SelectValue placeholder="Select an Industry" />
+                </SelectTrigger>
+                <SelectContent>
+                  {industries.map((ind) => {
+                    return(
+                      <SelectItem value={ind.id} key={ind.id}>{ind.name}</SelectItem>
+                    )
+                  })}
+                </SelectContent>
+              </Select>
+              {errors.industry && (
+                <p className="text-sm text-red-500">
+                  {errors.industry.message}
+                </p>
+              )}
+              </div>
+
+              {/* Sub-Industries */}
+
+            {watchIndustry && (
+                <div className="space-y-2">
+                <Label htmlFor="subIndustry">Specialization</Label>
+                <Select onValueChange={(value) => setValue("subIndustry",value)}>
+                  <SelectTrigger id="subIndustry">
+                    <SelectValue placeholder="Select Sub Industry" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {selectedIndustry?.subIndustries.map((ind) => {
+                      return(
+                        <SelectItem value={ind} key={ind}>{ind}</SelectItem>
+                      )
+                    })}
+                  </SelectContent>
+                </Select>
+                {errors.subIndustry && (
+                  <p className="text-sm text-red-500">
+                    {errors.subIndustry.message}
+                  </p>
+                )}
+                </div>
+              )
+            }
+
+            {/* Experience */}
+            <div className="space-y-2">
+              <Label htmlFor="experience">Experience</Label>
+              <Input
+                id="experience"
+                type="number"
+                min="0"
+                max="50"
+                placeholder="Enter years of experience"
+                {...register("experience")}
+              />
+              {errors.experience && (
+                <p className="text-sm text-red-500">
+                  {errors.experience.message}
+                </p>
+              )}
+            </div>
+
+            {/* Skills */}
+            <div className="space-y-2">
+              <Label htmlFor="skills">Skills</Label>
+              <Input
+                id="skills"
+                placeholder="e.g: Python, Java, Javascript, Project Management"
+                {...register("skills")}
+              />
+
+              <p className="text-sm text-muted-foreground">
+               Please separate multiple skills using commas for clarity and organization.
+              </p>
+
+              {errors.skills && (
+                <p className="text-sm text-red-500">
+                  {errors.skills.message}
+                </p>
+              )}
+            </div>
+
+            {/* Professional bio field */}
+            <div className="space-y-2">
+              <Label htmlFor="bio">Professional Bio</Label>
+              <Textarea
+                id="bio"
+                placeholder="Kindly share your professional bio..."
+                className="h-32"
+                {...register("bio")}
+              />
+
+              {errors.bio && (
+                <p className="text-sm text-red-500">
+                  {errors.bio.message}
+                </p>
+              )}
+            </div>
+
+            <Button type="submit" className="w-full">
+              Complete Profile
+            </Button>
+
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
-
 export default OnboardingForm
