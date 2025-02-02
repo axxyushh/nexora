fa585fcabc0a062c50763f4d390cbe93b11b5b65
ayush <flamedeveloper2021@gmail.com>
2025-02-02 10:44:37 +0530

    Created Routes for features.

--- middleware.js
+++ middleware.js
@@ -7,11 +7,12 @@ const isProtectedRoute = createRouteMatcher([
   "/interview(.*)",
   "/ai-cover-letter(.*)",
   "/onboarding(.*)",
-])
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = auth();
-  if(!userId && isProtectedRoute(req)){
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
@@ -22,8 +23,8 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
