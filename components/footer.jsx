bf739837df7fd0c8e31c44d5ec17bc287d7ce186
ayush <flamedeveloper2021@gmail.com>
2025-02-18 14:10:57 +0530

    short commit.

--- components/footer.jsx
+++ components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="container mx-auto px-4 text-center text-gray-200">
         <p className="text-l">© {new Date().getFullYear()} Nexora. All rights reserved.</p>
         <p className="mt-1 text-l">Made by Ayush Srivastava</p>
-        <div className="mt-3 flex justify-center space-x-4">
+        <div className="mt-3 flex justify-center space-x-4 animate-pulse">
           <Link href="https://www.instagram.com/axxyushh/#" target="_blank" rel="noopener noreferrer">
             <Instagram className="w-5 h-5 text-white-900 hover:text-white-700 transition" />
           </Link>
