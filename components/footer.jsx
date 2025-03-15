f17570be3164578f653ea4d0a3ac106a0d7f1e62
ayush <flamedeveloper2021@gmail.com>
2025-03-15 09:09:22 +0530

    Updated Footer and corrected Links and Layout.

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,7 +6,12 @@ export default function Footer() {
     <footer className="bg-muted/50 py-12">
       <div className="container mx-auto px-4 text-center text-gray-200">
         <p className="text-sm">© {new Date().getFullYear()} Nexora. All rights reserved.</p>
-        <p className="mt-1 text-xs">Developed by Ayush Srivastava</p>
+        <p className="mt-1 text-lg">
+          Developed by&nbsp;
+          <Link href={"https://my-portfolio-seven-sand-94.vercel.app/"} target="_blank" className="hover:underline"> 
+            Ayush Srivastava
+          </Link>
+        </p>
         <div className="mt-3 flex justify-center space-x-4">
           <Link className="animate-pulse hover:animate-none" href="https://www.instagram.com/axxyushh/#" target="_blank" rel="noopener noreferrer">
             <Instagram className="w-5 h-5 text-white-900 hover:text-white-700 transition" />
